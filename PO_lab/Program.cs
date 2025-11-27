d7f4a1bb84888271b659b71ed69755cf6306a1e9
sochv <socha.ppp@gmail.com>
2025-11-27 21:36:13 +0100

    edycja polecenia w master

--- PO_lab/Program.cs
+++ PO_lab/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Przeniosłam się do gałęzi master")
+Console.WriteLine("Przeniosłam się do gałęzi master");
